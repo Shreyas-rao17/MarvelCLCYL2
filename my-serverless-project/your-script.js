5e5f7afd1f72111bc1fd1c1b9d7c14e4a7aa59b9
ShreRao-17 <122039691+Shreyas-rao17@users.noreply.github.com>
2024-10-05 18:09:39 +0530

    Add files via upload

---
+++ my-serverless-project/your-script.js
@@ -0,0 +1,11 @@
+async function fetchData() {
+    try {
+        const response = await fetch('/api/hello');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchData();
